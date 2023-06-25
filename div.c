4616625df5523e1b84262070a9687ede96c9a35b
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-06-25 21:58:31 +0100

    Create div.c

---
+++ div.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * divide - Divides the second element by the top element of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number of the opcode being executed.
+ */
+
+void divide(stack_t **stack, unsigned int line_number)
+{
+        if (*stack == NULL || (*stack)->next == NULL)
+        {
+                fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        if ((*stack)->n == 0)
+        {
+                fprintf(stderr, "L%d: division by zero\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        (*stack)->next->n /= (*stack)->n;
+        pop(stack, line_number);
+}
