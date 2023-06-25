9791d4a9f5c10265ef4d6c4ed4e23d70162ef9db
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-06-25 22:08:02 +0100

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number of the opcode being executed.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+        (void)stack;
+        (void)line_number;
+        /* Does nothing */
+        /* Still doing nothing*/
+}
