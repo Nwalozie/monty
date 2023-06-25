8dfefcc4c59d6bf678c5cef817a64d5a07e60908
Nwalozie <iyekcnwalozie@gmail.com>
2023-06-25 16:46:04 -0700

    updated

--- execute_instruction.c
+++ execute_instruction.c
@@ -1,37 +1,37 @@
 #include "monty.h"
 
 void execute_instruction(stack_t **stack,
-                char *opcode, unsigned int line_number)
+		char *opcode, unsigned int line_number)
 {
-        if (strcmp(opcode, "push") == 0)
-                push(stack, line_number);
-        else if (strcmp(opcode, "pop") == 0)
-                pop(stack, line_number);
-        else if (strcmp(opcode, "pall") == 0 || strcmp(opcode, "pall$") == 0)
-                pall(stack, line_number);
-        else if (strcmp(opcode, "pint") == 0)
-                pint(stack, line_number);
-        else if (strcmp(opcode, "add") == 0)
-                add(stack, line_number);
-        else if (strcmp(opcode, "sub") == 0)
-                sub(stack, line_number);
-        else if (strcmp(opcode, "div") == 0)
-                divide(stack, line_number);
-        else if (strcmp(opcode, "swap") == 0)
-                swap(stack, line_number);
-        else if (strcmp(opcode, "nop") == 0)
-                nop(stack, line_number);
-        else if (strcmp(opcode, "mul") == 0)
-                mul(stack, line_number);
-        else if (strcmp(opcode, "mod") == 0)
-                mod(stack, line_number);
-        else if (strcmp(opcode, "rotr") == 0)
-                rotr(stack, line_number);
-        else if (strcmp(opcode, "pchr") == 0)
-                pchar(stack, line_number);
-        else
-        {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                exit(EXIT_FAILURE);
-        }
+	if (strcmp(opcode, "push") == 0)
+		push(stack, line_number);
+	else if (strcmp(opcode, "pop") == 0)
+		pop(stack, line_number);
+	else if (strcmp(opcode, "pall") == 0 || strcmp(opcode, "pall$") == 0)
+		pall(stack, line_number);
+	else if (strcmp(opcode, "pint") == 0)
+		pint(stack, line_number);
+	else if (strcmp(opcode, "add") == 0)
+		add(stack, line_number);
+	else if (strcmp(opcode, "sub") == 0)
+		sub(stack, line_number);
+	else if (strcmp(opcode, "div") == 0)
+		divide(stack, line_number);
+	else if (strcmp(opcode, "swap") == 0)
+		swap(stack, line_number);
+	else if (strcmp(opcode, "nop") == 0)
+		nop(stack, line_number);
+	else if (strcmp(opcode, "mul") == 0)
+		mul(stack, line_number);
+	else if (strcmp(opcode, "mod") == 0)
+		mod(stack, line_number);
+	else if (strcmp(opcode, "rotr") == 0)
+		rotr(stack, line_number);
+	else if (strcmp(opcode, "pchr") == 0)
+		pchar(stack, line_number);
+	else
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
 }
--- p_char.c
+++ p_char.c
@@ -8,20 +8,20 @@
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-                free_stack(stack);
-                exit(EXIT_FAILURE);
-        }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
 
-        if ((*stack)->n < 0 || (*stack)->n > 127)
-        {
-                fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
-                free_stack(stack);
-                exit(EXIT_FAILURE);
-        }
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
 
-        putchar((*stack)->n);
-        putchar('\n');
+	putchar((*stack)->n);
+	putchar('\n');
 }
