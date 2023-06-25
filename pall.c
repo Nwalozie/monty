d354f4999fd2cba620ee0f64d0c45c8b33389185
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-06-25 22:08:32 +0100

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all the elements of the stack.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number of the opcode being executed.
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+        stack_t *temp;
+        (void)line_number;
+
+        temp = *stack;
+        while (temp != NULL)
+        {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+        }
+}
