111927cd2c5fce1d8360eb2e251f81ce8e0b3c9b
Tboy54321 <122861724+Tboy54321@users.noreply.github.com>
2023-06-26 00:54:36 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -56,5 +56,6 @@ void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
