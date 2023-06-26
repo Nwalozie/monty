02864ae0d768f1856918bda8518cd59404f1c2bf
Nwalozie <iyekcnwalozie@gmail.com>
2023-06-25 17:10:26 -0700

    patch

--- pstr.c
+++ pstr.c
@@ -11,14 +11,14 @@
 
 void pstr(stack_t **stack, unsigned int line_number)
 {
-        stack_t *current = *stack;
-        (void)line_number;
+	stack_t *current = *stack;
+	(void)line_number;
 
-        while (current != NULL && current->n != 0 && current->n >= 32 && current->n <= 126)
-        {
-                putchar(current->n);
-                current = current->next;
-        }
+	while (current != NULL && current->n != 0 && current->n >= 32 && current->n <= 126)
+	{
+		putchar(current->n);
+		current = current->next;
+	}
 
-        putchar('\n');
+	putchar('\n');
 }
